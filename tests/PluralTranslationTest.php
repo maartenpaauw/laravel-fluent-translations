dae01b9bcebeea3892f0bca79d31f611e01da8fb
Maarten Paauw <maartenpaauw@gmail.com>
2022-10-22 15:31:02 +0200

    fix: test description

--- tests/PluralTranslationTest.php
+++ tests/PluralTranslationTest.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 use Maartenpaauw\Translation\PluralTranslation;
 
-it('should be possible to to generate singular and plural combined translation messages', function () {
+it('should be possible to generate singular and plural combined translation messages', function () {
     // Act
     $message = (new PluralTranslation())
         ->singular('There is one apple')
